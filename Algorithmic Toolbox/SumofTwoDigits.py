f833af123b011b5d085b55467c6c3e1ad91dcd80
Extomvi <exceltadedokun@gmail.com>
2022-01-12 21:51:15 +0100

    Updated code

--- Algorithmic Toolbox/SumofTwoDigits.py
+++ Algorithmic Toolbox/SumofTwoDigits.py
@@ -4,7 +4,7 @@ def SumofTwoNumbers(a,b):
     return a + b
 
 if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    #a, b = map(int, input().split())
+    #a = int(input())
+    #b = int(input())
+    a, b = map(int, input().split())
     print(SumofTwoNumbers(a,b))
