461e2874f3412a20eae5d8731772e02a301e5e3f
Extomvi <exceltadedokun@gmail.com>
2022-01-23 12:45:24 +0100

    Added fibonacci.py

--- Algorithmic Toolbox/fibonacci.py
+++ Algorithmic Toolbox/fibonacci.py
@@ -1,1 +1,25 @@
 # Fibonacci sequence
+
+def fibonacci(n):
+    a = 0
+    b = 1
+
+    if n < 0:
+        print("Incorrect input: n can't be less than 0")
+
+    elif n == 0:
+        return 0
+
+    elif n == 1:
+        return b
+
+    else:
+        for i in range(1, n):
+            c = a + b
+            a = b
+            b = c
+        return c
+
+     
+
+print(fibonacci(3))
